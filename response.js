67be1ef770e27949d0a8bcef1df33447182012c6
shravaniadakmol <shravaniadakmol5@gmail.com>
2023-07-06 21:15:06 +0530

    Initial Commit

---
+++ response.js
@@ -0,0 +1,14 @@
+const responseObj = {
+  hello: "Hey ! How are you doing ?",
+  hey: "Hey! How can I assist you?",
+  today: new Date().toDateString(),
+  time: new Date().toLocaleTimeString(),
+  "how are you ?":"Fine. What's up?",
+  hiee: "Hey! How it's going?",
+  great:"Good to hear that. How can I help you?",
+  fine:"Hope everything is on it's way.",
+  "what is your name?":"I am a Chatbot, You can call me El.",
+  "what is programming language": "A way for programmers to communicate with computers.",
+  "tell me a joke":"Why do programmers prefer dark mode? Because light attracts bugs!",
+  byee:"Goodbye. Have a great day.",
+  };
